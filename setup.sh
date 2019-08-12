976d44db29c85f87190183be82480d0e1656f00c
Monster <monster@cloud.local>
2019-08-12 13:09:16 +0000

    mispell

--- setup.sh
+++ setup.sh
@@ -9,22 +9,22 @@ function func_download_and_pack_kvm {
     wget -qO- /tmp $BASE_URL'/uefi.tar.gz' | tar xvz -C /tmp
 }
 
-function func_fownload_proxmox_iso {
+function func_download_proxmox_iso {
     wget http://download.proxmox.com/iso/proxmox-ve_6.0-1.iso -o /mnt/proxmox.iso
 }
 function func_run_qemu {
     /tmp/qemu-system-x86_64 -net nic -net user,hostfwd=tcp::3389-:3389 -m 102400M -localtime \ 
-    -enable-kvm -cpu host,+nx -M pc -smp 2 -vga std -usbdevice tablet -k en-us -cdrom /mnt/windows.iso -hda /dev/sda -hdb /dev/hdb -boot once=d -vnc :1
+    -enable-kvm -cpu host,+nx -M pc -smp 2 -vga std -usbdevice tablet -k en-us -cdrom /mnt/windows.iso -hda /dev/sda -hdb /dev/sdb -boot once=d -vnc :1
 }
 
 function func_run_qemu_uefi {
     /tmp/qemu-system-x86_64 -bios /tmp/uefi.bin -net nic -net user,hostfwd=tcp::3389-:3389 -m 102400M -localtime \ 
-    -enable-kvm -cpu host,+nx -M pc -smp 2 -vga std -usbdevice tablet -k en-us -cdrom /mnt/windows.iso -hda /dev/sda -hdb /dev/hdb -boot once=d -vnc :1
+    -enable-kvm -cpu host,+nx -M pc -smp 2 -vga std -usbdevice tablet -k en-us -cdrom /mnt/windows.iso -hda /dev/sda -hdb /dev/sdb -boot once=d -vnc :1
 }
 
 
 
 mount -t tmpfs -o size=6000m tmpfs /mnt
-func_fownload_proxmox_iso
+func_download_proxmox_iso
 func_download_and_pack_kvm
 func_run_qemu
