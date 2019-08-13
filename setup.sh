7f5ddec66d850d449642e2ddf0effebbff0fb4e2
Monster <monster@cloud.local>
2019-08-13 11:49:39 +0000

    erase disks && confirm

--- setup.sh
+++ setup.sh
@@ -22,8 +22,33 @@ function func_run_qemu_uefi {
     -enable-kvm -cpu host,+nx -M pc -smp 2 -vga std -usbdevice tablet -k en-us -cdrom /mnt/windows.iso -hda /dev/sda -hdb /dev/sdb -boot once=d -vnc :1
 }
 
+function func_erase_disks {
+    ATTRS=$(lsblk -d -n -l | grep -P " disk" | grep -E -o "(^sd[a-z]) ")
+    for i in "${ATTRS[@]}"
+    do
+	:
+	echo $i
+	#sfdisk $i < ""
+    done
+    #echo $ATTRS
+}
 
 
+func_prompt_confirm() {
+  lsblk -d | grep "^sd"
+  while true; do
+    read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
+    case $REPLY in
+      [yY]) echo ; return 0 ;;
+      [nN]) echo ; return 1 ;;
+      *) printf " \033[31m %s \n\033[0m" "invalid input"
+    esac 
+  done
+}
+
+
+func_prompt_confirm "This command will erase all your data on your disks. Are you shure?" || exit 0
+func_erase_disks
 mount -t tmpfs -o size=6000m tmpfs /mnt
 func_download_proxmox_iso
 func_download_and_unpack_kvm
